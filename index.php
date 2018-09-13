d88565732bfd0211b5b7c51b6f71531442bf66fa
pineapple333 <97928810w@gmail.com>
2018-09-13 19:07:39 +0300

    First practice lesson, a form and a session

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-  echo "Hello World"
+  session_start();
 ?>
-Something is wrong with the XAMPP installation :-(
+<form method="post"
+  action = "index.php">
+  
+  <input type="text"
+    name = "login">
+
+  <input type="password"
+    name = "password">
+
+  <input type = "Submit">
+
+</form>
+
+<?php
+  if (count($_POST)>0){
+	  if($_POST['password'] == 'password'){
+		  echo 'confirmed';
+		  $_SESSION['login'] = $_POST['login'];
+	  }
+  }
+
+?>
